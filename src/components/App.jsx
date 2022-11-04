83b5c7591aae8d5f70943cfa3ce1914d7cc99e71
Yurii-Naumchuk <98982620+Yurii-Naumchuk@users.noreply.github.com>
2022-11-04 14:21:31 +0200

    Profile done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,17 @@
-import Profile from "./Profile/Profile";
+import  Profile  from 'components/Profile/Profile.jsx';
+
+import user from 'components/Profile/user.json';
 
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-      <Profile/>
+    <div>
+    <Profile
+    username={user.username}
+    tag={user.tag}
+    location={user.location}
+    avatar={user.avatar}
+    stats={user.stats}
+  />
     </div>
   );
 };
